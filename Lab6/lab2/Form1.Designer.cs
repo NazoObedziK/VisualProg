b0f203b044c3dfc2f91ca7545705722a90ffeb02
NazoObedziK <79368444+NazoObedziK@users.noreply.github.com>
2022-06-24 18:46:22 +0300

    Add files via upload

---
+++ Lab6/lab2/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.Options = new System.Windows.Forms.Panel();
+			this.test = new System.Windows.Forms.Label();
+			this.Triangulation = new System.Windows.Forms.Button();
+			this.Clear = new System.Windows.Forms.Button();
+			this.PointsLabel = new System.Windows.Forms.Label();
+			this.NumOfPoints = new System.Windows.Forms.TextBox();
+			this.randPoints = new System.Windows.Forms.Button();
+			this.canvas = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.mark10 = new System.Windows.Forms.Label();
+			this.mark9 = new System.Windows.Forms.Label();
+			this.mark8 = new System.Windows.Forms.Label();
+			this.mark7 = new System.Windows.Forms.Label();
+			this.mark6 = new System.Windows.Forms.Label();
+			this.mark5 = new System.Windows.Forms.Label();
+			this.mark4 = new System.Windows.Forms.Label();
+			this.mark3 = new System.Windows.Forms.Label();
+			this.mark2 = new System.Windows.Forms.Label();
+			this.mark1 = new System.Windows.Forms.Label();
+			this.pointInside = new System.Windows.Forms.RichTextBox();
+			this.mousePos = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.Options.SuspendLayout();
+			this.canvas.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// Options
+			// 
+			this.Options.BackColor = System.Drawing.Color.White;
+			this.Options.Controls.Add(this.label2);
+			this.Options.Controls.Add(this.test);
+			this.Options.Controls.Add(this.Triangulation);
+			this.Options.Controls.Add(this.Clear);
+			this.Options.Controls.Add(this.PointsLabel);
+			this.Options.Controls.Add(this.NumOfPoints);
+			this.Options.Controls.Add(this.randPoints);
+			this.Options.Dock = System.Windows.Forms.DockStyle.Top;
+			this.Options.Location = new System.Drawing.Point(0, 0);
+			this.Options.Margin = new System.Windows.Forms.Padding(2);
+			this.Options.Name = "Options";
+			this.Options.Size = new System.Drawing.Size(946, 53);
+			this.Options.TabIndex = 0;
+			// 
+			// test
+			// 
+			this.test.AutoSize = true;
+			this.test.Location = new System.Drawing.Point(469, 23);
+			this.test.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.test.Name = "test";
+			this.test.Size = new System.Drawing.Size(0, 13);
+			this.test.TabIndex = 5;
+			// 
+			// Triangulation
+			// 
+			this.Triangulation.BackColor = System.Drawing.Color.Black;
+			this.Triangulation.FlatAppearance.BorderSize = 0;
+			this.Triangulation.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(11)))), ((int)(((byte)(18)))));
+			this.Triangulation.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.Triangulation.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.Triangulation.Location = new System.Drawing.Point(837, 15);
+			this.Triangulation.Margin = new System.Windows.Forms.Padding(2);
+			this.Triangulation.Name = "Triangulation";
+			this.Triangulation.Size = new System.Drawing.Size(100, 28);
+			this.Triangulation.TabIndex = 4;
+			this.Triangulation.Text = "Обчислити";
+			this.Triangulation.UseVisualStyleBackColor = false;
+			this.Triangulation.Click += new System.EventHandler(this.MakeTriangulation);
+			// 
+			// Clear
+			// 
+			this.Clear.BackColor = System.Drawing.Color.Black;
+			this.Clear.FlatAppearance.BorderSize = 0;
+			this.Clear.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(11)))), ((int)(((byte)(18)))));
+			this.Clear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.Clear.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.Clear.Location = new System.Drawing.Point(274, 15);
+			this.Clear.Margin = new System.Windows.Forms.Padding(2);
+			this.Clear.Name = "Clear";
+			this.Clear.Size = new System.Drawing.Size(86, 28);
+			this.Clear.TabIndex = 3;
+			this.Clear.Text = "Очистити";
+			this.Clear.UseVisualStyleBackColor = false;
+			this.Clear.Click += new System.EventHandler(this.ClearCanvas);
+			// 
+			// PointsLabel
+			// 
+			this.PointsLabel.AutoSize = true;
+			this.PointsLabel.BackColor = System.Drawing.Color.Black;
+			this.PointsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.PointsLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.PointsLabel.Location = new System.Drawing.Point(2, 18);
+			this.PointsLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.PointsLabel.Name = "PointsLabel";
+			this.PointsLabel.Size = new System.Drawing.Size(126, 20);
+			this.PointsLabel.TabIndex = 2;
+			this.PointsLabel.Text = "Кількість точок";
+			// 
+			// NumOfPoints
+			// 
+			this.NumOfPoints.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.NumOfPoints.Location = new System.Drawing.Point(141, 20);
+			this.NumOfPoints.Margin = new System.Windows.Forms.Padding(2);
+			this.NumOfPoints.Name = "NumOfPoints";
+			this.NumOfPoints.Size = new System.Drawing.Size(30, 23);
+			this.NumOfPoints.TabIndex = 1;
+			this.NumOfPoints.Text = "1";
+			this.NumOfPoints.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.NumOfPoints.TextChanged += new System.EventHandler(this.NumOfPoints_TextChanged);
+			// 
+			// randPoints
+			// 
+			this.randPoints.BackColor = System.Drawing.Color.Black;
+			this.randPoints.FlatAppearance.BorderSize = 0;
+			this.randPoints.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(11)))), ((int)(((byte)(18)))));
+			this.randPoints.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.randPoints.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.randPoints.Location = new System.Drawing.Point(184, 15);
+			this.randPoints.Margin = new System.Windows.Forms.Padding(2);
+			this.randPoints.Name = "randPoints";
+			this.randPoints.Size = new System.Drawing.Size(86, 28);
+			this.randPoints.TabIndex = 0;
+			this.randPoints.Text = "Згенерувати";
+			this.randPoints.UseVisualStyleBackColor = false;
+			this.randPoints.Click += new System.EventHandler(this.GeneratePoints);
+			// 
+			// canvas
+			// 
+			this.canvas.BackColor = System.Drawing.Color.White;
+			this.canvas.Controls.Add(this.label1);
+			this.canvas.Controls.Add(this.mark10);
+			this.canvas.Controls.Add(this.mark9);
+			this.canvas.Controls.Add(this.mark8);
+			this.canvas.Controls.Add(this.mark7);
+			this.canvas.Controls.Add(this.mark6);
+			this.canvas.Controls.Add(this.mark5);
+			this.canvas.Controls.Add(this.mark4);
+			this.canvas.Controls.Add(this.mark3);
+			this.canvas.Controls.Add(this.mark2);
+			this.canvas.Controls.Add(this.mark1);
+			this.canvas.Controls.Add(this.pointInside);
+			this.canvas.Controls.Add(this.mousePos);
+			this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.canvas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.canvas.Location = new System.Drawing.Point(0, 53);
+			this.canvas.Margin = new System.Windows.Forms.Padding(2);
+			this.canvas.Name = "canvas";
+			this.canvas.Size = new System.Drawing.Size(946, 556);
+			this.canvas.TabIndex = 1;
+			this.canvas.Click += new System.EventHandler(this.AddPoint);
+			this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Paint);
+			this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.GetMousePos);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(854, 14);
+			this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(68, 15);
+			this.label1.TabIndex = 18;
+			this.label1.Text = "Результат";
+			// 
+			// mark10
+			// 
+			this.mark10.AutoSize = true;
+			this.mark10.Location = new System.Drawing.Point(521, 343);
+			this.mark10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.mark10.Name = "mark10";
+			this.mark10.Size = new System.Drawing.Size(0, 15);
+			this.mark10.TabIndex = 17;
+			// 
+			// mark9
+			// 
+			this.mark9.AutoSize = true;
+			this.mark9.Location = new System.Drawing.Point(515, 336);
+			this.mark9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.mark9.Name = "mark9";
+			this.mark9.Size = new System.Drawing.Size(0, 15);
+			this.mark9.TabIndex = 16;
+			// 
+			// mark8
+			// 
+			this.mark8.AutoSize = true;
+			this.mark8.Location = new System.Drawing.Point(509, 330);
+			this.mark8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.mark8.Name = "mark8";
+			this.mark8.Size = new System.Drawing.Size(0, 15);
+			this.mark8.TabIndex = 15;
+			// 
+			// mark7
+			// 
+			this.mark7.AutoSize = true;
+			this.mark7.Location = new System.Drawing.Point(503, 323);
+			this.mark7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.mark7.Name = "mark7";
+			this.mark7.Size = new System.Drawing.Size(0, 15);
+			this.mark7.TabIndex = 14;
+			// 
+			// mark6
+			// 
+			this.mark6.AutoSize = true;
+			this.mark6.Location = new System.Drawing.Point(497, 317);
+			this.mark6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.mark6.Name = "mark6";
+			this.mark6.Size = new System.Drawing.Size(0, 15);
+			this.mark6.TabIndex = 13;
+			// 
+			// mark5
+			// 
+			this.mark5.AutoSize = true;
+			this.mark5.Location = new System.Drawing.Point(491, 310);
+			this.mark5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.mark5.Name = "mark5";
+			this.mark5.Size = new System.Drawing.Size(0, 15);
+			this.mark5.TabIndex = 12;
+			// 
+			// mark4
+			// 
+			this.mark4.AutoSize = true;
+			this.mark4.Location = new System.Drawing.Point(485, 304);
+			this.mark4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.mark4.Name = "mark4";
+			this.mark4.Size = new System.Drawing.Size(0, 15);
+			this.mark4.TabIndex = 11;
+			// 
+			// mark3
+			// 
+			this.mark3.AutoSize = true;
+			this.mark3.Location = new System.Drawing.Point(479, 297);
+			this.mark3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.mark3.Name = "mark3";
+			this.mark3.Size = new System.Drawing.Size(0, 15);
+			this.mark3.TabIndex = 10;
+			// 
+			// mark2
+			// 
+			this.mark2.AutoSize = true;
+			this.mark2.Location = new System.Drawing.Point(473, 291);
+			this.mark2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.mark2.Name = "mark2";
+			this.mark2.Size = new System.Drawing.Size(0, 15);
+			this.mark2.TabIndex = 9;
+			// 
+			// mark1
+			// 
+			this.mark1.AutoSize = true;
+			this.mark1.Location = new System.Drawing.Point(212, 102);
+			this.mark1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.mark1.Name = "mark1";
+			this.mark1.Size = new System.Drawing.Size(0, 15);
+			this.mark1.TabIndex = 8;
+			// 
+			// pointInside
+			// 
+			this.pointInside.BackColor = System.Drawing.SystemColors.Window;
+			this.pointInside.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pointInside.Location = new System.Drawing.Point(837, 31);
+			this.pointInside.Margin = new System.Windows.Forms.Padding(2);
+			this.pointInside.Name = "pointInside";
+			this.pointInside.Size = new System.Drawing.Size(102, 528);
+			this.pointInside.TabIndex = 7;
+			this.pointInside.Text = "";
+			// 
+			// mousePos
+			// 
+			this.mousePos.AutoSize = true;
+			this.mousePos.Location = new System.Drawing.Point(903, 574);
+			this.mousePos.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.mousePos.Name = "mousePos";
+			this.mousePos.Size = new System.Drawing.Size(0, 15);
+			this.mousePos.TabIndex = 6;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.BackColor = System.Drawing.Color.Black;
+			this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.label2.Location = new System.Drawing.Point(425, 9);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(96, 13);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "number of triangles";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(946, 609);
+			this.Controls.Add(this.canvas);
+			this.Controls.Add(this.Options);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Margin = new System.Windows.Forms.Padding(2);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Lab2";
+			this.Options.ResumeLayout(false);
+			this.Options.PerformLayout();
+			this.canvas.ResumeLayout(false);
+			this.canvas.PerformLayout();
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Options;
+        public System.Windows.Forms.Panel canvas;
+        private System.Windows.Forms.TextBox NumOfPoints;
+        private System.Windows.Forms.Button randPoints;
+        private System.Windows.Forms.Label PointsLabel;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Triangulation;
+        private System.Windows.Forms.Label mousePos;
+        private System.Windows.Forms.RichTextBox pointInside;
+        private System.Windows.Forms.Label mark1;
+        private System.Windows.Forms.Label mark10;
+        private System.Windows.Forms.Label mark9;
+        private System.Windows.Forms.Label mark8;
+        private System.Windows.Forms.Label mark7;
+        private System.Windows.Forms.Label mark6;
+        private System.Windows.Forms.Label mark5;
+        private System.Windows.Forms.Label mark4;
+        private System.Windows.Forms.Label mark3;
+        private System.Windows.Forms.Label mark2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label test;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
---
+++ Lab6/lab2/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+   
+    public partial class Form1 : Form
+    {
+        public Pen myPen = new Pen(Color.Black);
+        public Pen pointCol = new Pen(Color.Red);
+        public Graphics graphics = null;
+        public List<Point> points=new List<Point>();
+        private int pointSize = 3;
+        private Point newPoint;
+
+        private int xOffset;
+        private int yOffset;
+        //список для усіх утвр трикутн
+        public List<List<Point>> trianglesList=new List<List<Point>>();
+        //наші трикутники, що не містять точок всередині, і які ми виводимо і рисуємо
+        public List<List<Point>> trianglesWithoutPoints = new List<List<Point>>();
+        //список лейблів для надписів над точками 
+        //окей, їх буде 10, якщо більше точок - програмі важко рахувати/малювати, тому 
+        //можуть виникнути проблеми з програмою, якщо побудовано більше 10 точок, тоді графік не виведеться, лише точки, що утворюють трикутники
+        public List<Label> marks;
+		
+        private int index = 0;
+ 
+       
+        
+        public Form1()
+        {
+            InitializeComponent();
+            graphics = canvas.CreateGraphics();
+            myPen.Width = 1;
+            pointCol.Width = 3;
+            //задаємо власне ті 10 лейблів
+            marks = new List<Label>() { mark1, mark2, mark3,mark4,mark5,mark6,mark7,mark8,mark9,mark10 };
+            index = 0;
+           
+        }
+
+        private void Paint(object sender, PaintEventArgs e)
+        {
+            
+             
+        }
+        //додавання нової точки на нашу панель
+        private void AddPoint(object sender, EventArgs e)
+        {
+            xOffset = Location.X + canvas.Location.X + 12;
+            yOffset = Location.Y + canvas.Location.Y + 35;
+            //додаємо точку біля курсора,  xOffset і  уOffset потрібі, щоб вище точки поставити лейбл з її координатами
+            newPoint = new Point(Cursor.Position.X-xOffset,Cursor.Position.Y -yOffset );
+            points.Add(newPoint);
+            //додаємо лейбл з позицією над точкою 
+            MarkPoint(newPoint);
+
+            ShowPoints();
+          
+
+        }   
+       
+       //побудова ліній , з яких з'єднаються трикутники
+       //будуємо всеможливі лінії з усеможливих наборів точок по 2
+        public void ConnectAllPoints(List<Point> points)
+        {
+            //логічно, що для побудови лінії нам треба мати більше,ніж дві точки
+            if (points.Count >= 2)
+            {
+                for (int i = 0; i < points.Count - 1; i++)
+                {
+					for (int j = 0; j < points.Count; j++)
+					{
+                        //власне рисування лінії
+						graphics.DrawLine(myPen, points[i], points[j]);
+					}
+                }
+            }
+        }
+      //генерування точок рандомом (в нас є відповідна кнопка)
+        public void GeneratePoints(List<Point>points,int numOfPoints)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < numOfPoints; i++)
+            {
+                int randX = rand.Next(canvas.Location.X, canvas.Location.X + canvas.Width - 140);
+                int randY = rand.Next(canvas.Location.Y - 50, canvas.Location.Y + canvas.Height - 60);
+                points.Add(new Point(randX, randY));
+            }
+            
+        }
+
+        public void ShowPoints()
+        {
+            for (int i = 0; i < points.Count; i++)
+            {
+                graphics.DrawEllipse(pointCol, points[i].X, points[i].Y, pointSize, pointSize);
+            }
+        }
+        //генерування точок+ лейбли над ними з підписами позицій
+        private void GeneratePoints(object sender, EventArgs e)
+        {
+            GeneratePoints(points, Int32.Parse(NumOfPoints.Text));
+            ShowPoints();
+            for (int i = 0; i < points.Count; i++)
+            {
+                MarkPoint(points[i]);
+                if (index>marks.Count)
+                {
+                    
+                    break;
+                }
+            }
+        }
+
+        private void ClearCanvas(object sender, EventArgs e)
+        {  // по суті, для очищення панелі видаляємо точки з допом методу RemoveRange,де 0- індекс,з якого починаємо видаляти
+            //і оці всі каунти- кількість точок ,яку треба видалити
+            //тобто ми просто очищаємо всі створені нами вище списки
+            points.RemoveRange(0,points.Count);
+            trianglesList.RemoveRange(0,trianglesList.Count);
+            trianglesWithoutPoints.RemoveRange(0, trianglesWithoutPoints.Count);
+            pointInside.Text = "";
+
+            for (int i = 0; i < marks.Count; i++)
+            {
+                //очищаємо текст з лейблів над точками( де пише їх розташування)
+                marks[i].Text = "";
+            }
+            index = 0;
+            //очищаємо сам елемент graphics
+            graphics.Clear(Color.White);
+        }
+        //це не зовсім алгоритм тріангуляції ,але деякі функції звідти можна взяти
+        private void MakeTriangulation(object sender, EventArgs e)
+        {//створюєм/додаєм список всеможливих трикутників
+            AddAllTrianglesToTheList();
+ 
+            test.Text = trianglesList.Count.ToString();
+            //якщо точок тільки три, це автоматоматично трикутник без точок всередині
+            if (points.Count==3)
+            {
+                trianglesWithoutPoints.Add(points);
+            }
+            else
+            {
+                for (int i = 0; i < points.Count; i++)
+                {//інакше викликаємо функцію для відбору трикутників, всередині яких точок немає
+                    TrianglesWithoutPointInside(points[i]);
+                }
+               
+            }
+            for (int i = 0; i < trianglesWithoutPoints.Count; i++)
+            {//тут для всіх знайдених трикутників виводяться точки,на яких вони побудовані в нашому RichTextBox pointInside
+                for (int j = 0; j < 3; j++)
+                { 
+                    pointInside.Text += trianglesWithoutPoints[i][j] + "\n";
+                }
+                pointInside.Text += "\n";
+            }
+
+            //з'єднуємо лінії для побудови трикутників
+            ConnectAllPoints(points);
+
+        }
+        //якщо точка не всередині котрогось з трикутників, трикутник додається до списку трикутників без точок всередині
+        public void AllTriangles(List<Point> pointsIn)
+        {
+            for (int i = 0; i < trianglesList.Count; i++)
+            {
+                for (int j = 0; j < trianglesList[i].Count; j++)
+                {
+                    for (int k = 0; k < pointsIn.Count; k++)
+                    {
+                        if (pointsIn[k] != trianglesList[i][j])
+                        {
+                            trianglesWithoutPoints.Add(trianglesList[i]);
+                        }
+                    }
+                   
+                }
+                
+            }
+        }
+        //виведення по три точки, що утворюють трикутник в річтекстбокс 
+        //тобто виводимо інформацію про отримані трикутники
+        public void ShowAllTriangles(List<List<Point>>trianglesWithoutPoints)
+        {
+            for (int i = 0; i < trianglesWithoutPoints.Count; i++)
+            {
+                for (int j = 0; j < trianglesWithoutPoints[i].Count; j++)
+                {
+                    pointInside.Text += trianglesWithoutPoints[i][j].X.ToString() + "," + trianglesWithoutPoints[i][j].Y.ToString()+"\n";
+                }
+                pointInside.Text += "\n";
+            }
+        }
+        //функція, запозичена з тріангуляції
+        //визнача чи є всередині трикутника , сформованого з трьох точок, наша точка point
+        //тобто ми перевіряємо всі трикутники, чи не знаходиться наша точка всередині
+        public bool PointInTriangle(List<Point> triangle,Point point)
+        {
+            var s = triangle[0].Y * triangle[2].X - triangle[0].X * triangle[2].Y + (triangle[2].Y - triangle[0].Y) * point.X + 
+				(triangle[0].X - triangle[2].X) * point.Y;
+            var t = triangle[0].X * triangle[1].Y - triangle[0].Y * triangle[1].X + (triangle[0].Y - triangle[1].Y) * point.X + 
+				(triangle[1].X - triangle[0].X) * point.Y;
+
+            if ((s < 0) != (t < 0))
+                return false;
+               
+            var A = -triangle[1].Y * triangle[2].X + triangle[0].Y * (triangle[2].X - triangle[1].X) + triangle[0].X * 
+				(triangle[1].Y - triangle[2].Y) + triangle[1].X * triangle[2].Y;
+
+            return  A < 0 ? (s <= 0 && s + t >= A) : (s >= 0 && s + t <= A);
+     
+        }
+        //знаходження трикутників без точок всередині( власне тих,які ми шукаємо з умови лр)
+        private void TrianglesWithoutPointInside(Point point)
+        {
+            byte isVertex = 1;
+            for (int i = 0; i < trianglesList.Count; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    //
+                    if (trianglesList[i][j].X != point.X && trianglesList[i][j].Y != point.Y)
+                        isVertex *= 1;
+                    else isVertex *= 0;
+                }
+                if (isVertex==1) //якщо наш прапорець спрацював, точок всередині немає
+                {  
+                    if (!PointInTriangle(trianglesList[i], point))
+                    {//додаємо в список трик без точок всередині
+                        trianglesWithoutPoints.Add(trianglesList[i]);
+                    }
+                }
+                isVertex = 1;
+            }
+           
+        }
+        //додаємо всі утворені нами трикутники в список
+        public void AddAllTrianglesToTheList()
+        {
+            //якщо точок більше 2, ми можемо намалювати для початку хоч лінії, з яких ми  будемо робити трикутники
+            if (points.Count>2)
+            {
+                for (int i = 0; i < points.Count; i++)
+                {
+                    for (int j = i + 1; j < points.Count; j++)
+                    {
+                        for (int k = j + 1; k < points.Count; k++)
+                        {
+                            //створюємо всеможливі трикутники по три точки і додаємо список точок, що відповідає трикутнику в список трикутників
+                            List<Point> newTriangle = new List<Point>();
+                            newTriangle.Add(points[i]);
+                            newTriangle.Add(points[j]);
+                            newTriangle.Add(points[k]);
+                            trianglesList.Add(newTriangle);
+                        }
+                    }
+                }
+            }
+            
+        }
+        //функція для отримання позиції миші
+        //
+        private void GetMousePos(object sender, MouseEventArgs e)
+        {
+            xOffset = Location.X + canvas.Location.X + 12;
+            yOffset = Location.Y + canvas.Location.Y + 35;
+            mousePos.Text = (Cursor.Position.X-xOffset).ToString()+","+(Cursor.Position.Y-yOffset).ToString();
+        }
+        private void MarkPoint(Point point)
+        {//текст на лейблах над точками, який показує координати точки на панелі рисування
+            marks[index].Text = "(" + point.X.ToString() + ";" + point.Y.ToString() + ")";
+            marks[index].Location = new Point(point.X, point.Y+10);
+            
+            index++;
+        }
+
+        private void NumOfPoints_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
